780893c1756ce2966abaedcae91f161976b19268
Guest User <Guest@Epicodus-6B.local>
2017-09-12 16:22:47 -0700

    complete project with multiple promises

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -3,61 +3,148 @@
 // var apiKey = require('./../.env/').apiKey;
 // $(document).ready(function(){
 //   $('#weatherLocation').click(function() {
-//       let city = $('#location').val();
-//       $('#location').val("");
+//     let city = $('#location').val();
+//     $('#location').val("");
 //
 //
-//       let request = new XMLHttpRequest();
-//       let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+//     let request = new XMLHttpRequest();
+//     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 //
-//       request.onreadystatechange = function() {
-//         if(this.readyState === 4 && this.status === 200) {
-//           let response = JSON.parse(this.responseText);
-//           populateFrontEnd(response);
-//         } else {
-//           $('#showErrors').text("There was an error processing your request. Please try again.")
-//         }
+//     request.onreadystatechange = function() {
+//       if(this.readyState === 4 && this.status === 200) {
+//         let response = JSON.parse(this.responseText);
+//         populateFrontEnd(response);
+//       } else {
+//         $('#showErrors').text("There was an error processing your request. Please try again.")
 //       }
+//     }
 //
-//       request.open("GET", url, true);
-//       request.send();
+//     request.open("GET", url, true);
+//     request.send();
 //
-//       var populateFrontEnd = function(response) {
-//         $('#showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-//         $('#showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-//       }
-//     });
+//     var populateFrontEnd = function(response) {
+//       $('#showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//       $('#showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+//     }
 //   });
+// });
 
-
-//Jquery AJAX api call
+//Vanilla JS API call with promises
 
 var apiKey = require('./../.env/').apiKey;
 $(document).ready(function(){
   $('#weatherLocation').click(function() {
-      let city = $('#location').val();
-      console.log(city);
-      // let url = "http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=e241fea983043a22d35a1c366f5a9058"
+    let city = $('#location').val();
+    $('#location').val("");
 
-      $('#location').val("");
-
-      $.ajax({
-        url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
-        type: 'GET',
-        data: {
-          format: 'json'
-        },
-        success: function(response) {
-          console.log("success");
-          $('#showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-          $('#showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-        },
-        error: function() {
-          console.log("error");
-          // $('#showErrors').text("There was an error processing your request. Please try again.")
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
         }
-      });
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    let promise2 = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=Bend&appid=${apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    let promises = [promise,promise2];
+    // console.log("OUT"+promises);
+
+    Promise.all(promises)
+      .then(function(responses) {
+        // console.log("IN then" +promises);
+        // console.log("IN thenresponses"+responses);
+
+        responses.forEach(function(response){
+          var populateFrontEnd = JSON.parse(response);
+          console.log(populateFrontEnd);
+          $('#showHumidity').append(`The humidity in ${populateFrontEnd.name} is ${populateFrontEnd.main.humidity}%<br>`);
+          $('#showTemp').append(`The temperature in ${populateFrontEnd.name} is ${populateFrontEnd.main.temp}<br>` );
+        });
 
 
+    // function(error) {
+    //   $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    // );
     });
   });
+});
+
+
+//Jquery AJAX api call
+
+// var apiKey = require('./../.env/').apiKey;
+// $(document).ready(function(){
+//   $('#weatherLocation').click(function() {
+//       let city = $('#location').val();
+//       console.log(city);
+//       // let url = "http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=e241fea983043a22d35a1c366f5a9058"
+//
+//       $('#location').val("");
+//
+//       $.ajax({
+//         url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
+//         type: 'GET',
+//         data: {
+//           format: 'json'
+//         },
+//         success: function(response) {
+//           $('#showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//           $('#showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+//         },
+//         error: function() {
+//           $('#showErrors').text("There was an error processing your request. Please try again.")
+//         }
+//       });
+//
+//
+//     });
+//   });
+
+//Jquery AJAX api call with promises
+//
+// var apiKey = require('./../.env/').apiKey;
+// $(document).ready(function(){
+//   $('#weatherLocation').click(function() {
+//       let city = $('#location').val();
+//       console.log(city);
+//       // let url = "http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=e241fea983043a22d35a1c366f5a9058"
+//
+//       $('#location').val("");
+//
+//       $.ajax({
+//         url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
+//         type: 'GET',
+//         data: {
+//           format: 'json'
+//         },
+//         success: function(response) {
+//           $('#showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//           $('#showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+//         },
+//         error: function() {
+//           $('#showErrors').text("There was an error processing your request. Please try again.")
+//         }
+//       });
+//
+//
+//     });
+//   });
