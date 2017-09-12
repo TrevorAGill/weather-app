32f70f769a10823e96fa3579194b876b69e5168e
Guest User <Guest@Epicodus-6B.local>
2017-09-12 13:30:17 -0700

    include functioning api calls using both jquery AJAX and vanilla JS

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,60 +1,63 @@
-var apiKey = require('./../.env/').apiKey;
-$(document).ready(function(){
-  $('#weatherLocation').click(function() {
-      let city = $('#location').val();
-      $('#location').val("");
-
-      let request = new XMLHttpRequest();
-      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
-      request.onreadystatechange = function() {
-        if(this.readyState === 4 && this.status === 200) {
-          let response = JSON.parse(this.responseText);
-          getElements(response);
-        }
-        // } else {
-        //   $('#showErrors').text("There was an error processing your request. Please try again.")
-        // }
-      }
-
-      request.open("GET", url, true);
-      request.send();
-
-      let getElements = function(response) {
-        $('#showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-        $('#showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-      }
-    });
-  });
-
-
+//Vanilla JS api call
 
 // var apiKey = require('./../.env/').apiKey;
 // $(document).ready(function(){
 //   $('#weatherLocation').click(function() {
 //       let city = $('#location').val();
-//       console.log(city);
-//       // let url = "http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=e241fea983043a22d35a1c366f5a9058"
-//
 //       $('#location').val("");
 //
-//       $.ajax({
-//         url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
-//         type: 'GET',
-//         data: {
-//           format: 'json'
-//         },
-//         success: function(response) {
-//           console.log("success");
-//           $('#showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-//           $('#showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-//         },
-//         error: function() {
-//           console.log("error");
-//           // $('#showErrors').text("There was an error processing your request. Please try again.")
+//
+//       let request = new XMLHttpRequest();
+//       let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+//
+//       request.onreadystatechange = function() {
+//         if(this.readyState === 4 && this.status === 200) {
+//           let response = JSON.parse(this.responseText);
+//           populateFrontEnd(response);
+//         } else {
+//           $('#showErrors').text("There was an error processing your request. Please try again.")
 //         }
-//       });
+//       }
 //
+//       request.open("GET", url, true);
+//       request.send();
 //
+//       var populateFrontEnd = function(response) {
+//         $('#showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//         $('#showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+//       }
 //     });
 //   });
+
+
+//Jquery AJAX api call
+
+var apiKey = require('./../.env/').apiKey;
+$(document).ready(function(){
+  $('#weatherLocation').click(function() {
+      let city = $('#location').val();
+      console.log(city);
+      // let url = "http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=e241fea983043a22d35a1c366f5a9058"
+
+      $('#location').val("");
+
+      $.ajax({
+        url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
+        type: 'GET',
+        data: {
+          format: 'json'
+        },
+        success: function(response) {
+          console.log("success");
+          $('#showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+          $('#showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+        },
+        error: function() {
+          console.log("error");
+          // $('#showErrors').text("There was an error processing your request. Please try again.")
+        }
+      });
+
+
+    });
+  });
